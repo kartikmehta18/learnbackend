3c23f13cb06c576cd5a2a22481c78f3f06540476
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-09-08 01:25:53 +0530

    mongo

--- part8/app.js
+++ part8/app.js
@@ -1,25 +1,51 @@
 const express = require("express");
 const app = express();
-const userModel = require("./usermodel")
+const userModel = require("./usermodel");
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.get('/',( req ,res) =>{
-    res.send('Hello World');
-})
+app.get("/creat", async (req, res) => {
+  let creat = await userModel.create({
+    name: "shivani",
+    username: "shiv",
+    email: "shivanimehta650@gmail.com",
+  });
+  res.send(creat);
+  console.log("Data created");
+});
 
-app.get('/creat', async( req ,res) =>{
-   let creat= await userModel.create({
-        name: 'kartik',
-        age: 21,
-        email: 'kartikmehta650@gmail.com'
-    })
-    res.send(creat)
-    console.log('Data created');
-})
+app.get("/read", async (req, res) => {
+  let user = await userModel.find();
+  res.send(user);
+});
 
+app.get("/oneread", async (req, res) => {
+  let user = await userModel.findOne({ username: "shiv" });
+  res.send(user);
+});
 
+app.get("/update", async (req, res) => {
+  let update = await userModel.findOneAndUpdate(
+    { username: "kartikmehta18" },
+    { name: "kartik mehta" },
+    { new: true }
+  );
+  res.send(update);
+  console.log("Data created");
+});
 
+app.get("/oneread", async (req, res) => {
+  let user = await userModel.findOne({ username: "shiv" });
+  res.send(user);
+});
 
-app.listen(3000 , function(){
-    console.log('server is running on port 3000');
-})
+app.get("/delete", async (req, res) => {
+  let delet = await userModel.findOneAndDelete({ name: "kartik" });
+  res.send(delet);
+});
+
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
+});
