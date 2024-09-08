6e6e79f86e3e2ae023c8945e6d7f8c7f2107aff0
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-09-09 00:05:43 +0530

    mongo

--- part9/app.js
+++ part9/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.use("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
     res.send('hellow world');
 })
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000');
+});
