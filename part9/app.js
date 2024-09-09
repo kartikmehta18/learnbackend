8de2322432c11251901e6c1b495d7a1c1d1dc721
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-09-10 00:06:54 +0530

    mongo

--- part9/app.js
+++ part9/app.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const path = require('path');
+
+// npm init -y
+// nom install express
+// npm i nodemon -g
+// nodemon script.js
+// npm i ejs
+// setup ejS AS A middelware for view engine
+
+const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use("view engine","ejs");
+//render ejs file
+app.set("view engine", "ejs");
+
+// handel foe json data or form data
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/',(req,res)=>{
-    res.send('hellow world');
-})
+app.get("/", function (req, res) {
+  //   res.send("hello devs");
+  res.render("index");
+});
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000');
+
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
 });
