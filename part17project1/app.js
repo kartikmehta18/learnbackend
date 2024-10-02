dd0860b13a96923544ebf81716deaa5002cc117c
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-10-02 22:17:13 +0530

    update

---
+++ part17project1/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const userModel = require("./models/user");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.get("/", (res, req) => {
+  req.render("index");
+});
+
+app.post("/register", async (req, res) => {
+  let user = await userModel.findOne({email: req.body.email});
+  if (user) return res.send("User already registered");
+
+  bcrypt.genSalt(10, (err, salt) => {}
+  //  userModel.create({
+  //   usernsame: req.body.username,
+  //   name: req.body.name,
+  //   age: req.body.age,
+  //   email: req.body.email,
+  //   password: req.body. password,
+  // });
+});
+
+app.listen(3000);
