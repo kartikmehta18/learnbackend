bfac566f4cdc09cd81aeb1ca7223e5b835985691
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-10-19 23:51:17 +0530

    backend

--- part17project1/app.js
+++ part17project1/app.js
@@ -13,22 +13,22 @@ app.get("/", (res, req) => {
 });
 
 app.post("/register", async (req, res) => {
-  let ={username, name, age, email, password} = req.body;
-  let user = await userModel.findOne({email: req.body.email});
+  let = { username, name, age, email, password } = req.body;
+  let user = await userModel.findOne({ email: req.body.email });
   if (user) return res.send("User already registered");
 
   bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(password, salt, (err,hash)=>{
+    bcrypt.hash(password, salt,async  (err, hash) => {
       
-    })
-  })
-  //  userModel.create({
-  //   usernsame: req.body.username,
-  //   name: req.body.name,
-  //   age: req.body.age,
-  //   email: req.body.email,
-  //   password: req.body. password,
-  // });
+      let user = await userModel.create({
+        username,
+        name,
+        age,
+        email,
+        password,
+      });
+    });
+  });
 });
 
 app.listen(3000);
--- part17project1/models/user.js
+++ part17project1/models/user.js
@@ -8,11 +8,17 @@ mongoose
   .catch((error) => console.error("MongoDB connection error:", error));
 
 const userSchema = new mongoose.Schema({
-  usernsame: String,
+  username: String,
   name: String,
   age: Number,
   email: String,
   password: String,
+  post:[
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ]
 });
 
 module.exports = mongoose.model("user", userSchema);
