4e30aebdf7cfab499a4c1f757ad3f0b994c66365
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-10-20 14:56:00 +0530

    backend

--- part17project1/app.js
+++ part17project1/app.js
@@ -5,6 +5,7 @@ const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken"); // Import jwt
+const { console } = require("inspector");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -14,7 +15,7 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.get("/login", (req, res) => {
+app.get("/login",isLoggedIn, (req, res) => {
   res.render("login");
 });
 
@@ -41,15 +42,34 @@ app.post("/register", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  let user = await userModel.findOne({ username });
-  if (user) return res.status(400).send("User already registered");
+  let user = await userModel.findOne({email});
+  if (!user) return res.status(500).send("something went wrong");
 
   bcrypt.compare(password,user.password , function (err,result) {
-   
+    if(result){
+      let token = jwt.sign({ email: email, userid: user._id }, "shh");
+  res.cookie("jwt", token);
+  res.send("User logged in");
+    }
+   else res.redirect('/login')
   });
+
+  
 });
+
+app.get("/logout", (req, res) => {  
+  console.log(res.cookies);
+  res.cookie("token",'');
+  console.log(res.cookies);
+  res.redirect("/login");
+});
+
+function isLoggedIn(req, res, next) {
+  console.log(req.cookies);
+  next();
+}
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
