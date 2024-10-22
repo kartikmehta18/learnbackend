ee84b675b25681b751f3dfa45f2cf43bde71b26d
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-10-22 19:22:01 +0530

    update

--- part17project1/app.js
+++ part17project1/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.get("/login",isLoggedIn, (req, res) => {
+app.get("/login", isLoggedIn, (req, res) => {
   res.render("login");
 });
 
@@ -35,7 +35,7 @@ app.post("/register", async (req, res) => {
       });
 
       let token = jwt.sign({ email: email, userid: user._id }, "shh");
-      res.cookie("jwt", token);
+      res.cookie("token", token);
       res.send("User registered");
     });
   });
@@ -44,25 +44,18 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
-  let user = await userModel.findOne({email});
+  let user = await userModel.findOne({ email });
   if (!user) return res.status(500).send("something went wrong");
 
-  bcrypt.compare(password,user.password , function (err,result) {
-    if(result){
-      let token = jwt.sign({ email: email, userid: user._id }, "shh");
-  res.cookie("jwt", token);
-  res.send("User logged in");
-    }
-   else res.redirect('/login')
+  bcrypt.compare(password, user.password, function (err, result) {
+    if (result) res.status(200).send("you can login");
+    else res.redirect("/login");
   });
-
-  
 });
 
-app.get("/logout", (req, res) => {  
-  console.log(res.cookies);
-  res.cookie("token",'');
-  console.log(res.cookies);
+app.get("/logout", (req, res) => {
+  console.log(res.cookie);
+  res.cookie("token", "");
   res.redirect("/login");
 });
 
