9f65942cd40778eb0049b7bbac7bbb18da128cbd
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-11-08 00:40:04 +0530

    project

--- part17project1/app.js
+++ part17project1/app.js
@@ -10,9 +10,7 @@ const jwt = require("jsonwebtoken"); // Import jwt
 const path = require("path");
 // const multer = require("multer");
 
-app.set("view engine", "ejs");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
