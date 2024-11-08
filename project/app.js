4cce8d0fd1bdc04eff33e655859d35d9254d38bb
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-11-09 01:06:58 +0530

    project

--- project/app.js
+++ project/app.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.send("Jai Jinendra ");
-});
+res.send("Jai Jinendradee ");
+});3
 
 app.listen(3000, function () {
   console.log("server is running on port 3000");
