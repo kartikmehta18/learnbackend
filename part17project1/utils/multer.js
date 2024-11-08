d7f124e2a2e41c3ac8f28ec8f806279825623435
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-11-08 20:30:28 +0530

    project

--- part17project1/utils/multer.js
+++ part17project1/utils/multer.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/images");
   },
-  filename: function (req, file, cb) {
+  filename: function (req, file, cb) { 
     // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
     crypto.randomBytes(12, function (err, bytes) {
       const fn = bytes.toString("hex") + path.extname(file.originalname);
