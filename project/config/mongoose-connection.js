c14b121ffe1f2ce1e89838abf1f463e9debd0128
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-11-11 00:10:05 +0530

    projrct

--- project/config/mongoose-connection.js
+++ project/config/mongoose-connection.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
+const config = require("config");
 const dbgr = require("debug")("development:mongoose");
 
-mongoose.connect("mongodb://localhost:27017/config").then(function () {
-  // console.log("Connected to database");
-  dbgr("Connected to database");
-})
-.catch(function (error) {
-  console.log("Error connecting to database");
-  console.log(error);
-});
+// $env:DEBUG="development:mongoose"
+mongoose
+  .connect(`${config.get("MONGODB_URI")}/project1`)
+  .then(function () {
+    // console.log("Connected to database");
+    dbgr("Connected to database");
+  })
+  .catch(function (error) {
+    console.log("Error connecting to database");
+    console.log(error);
+  });
 
 module.exports = mongoose.connection;
--- project/routes/ownerRouter.js
+++ project/routes/ownerRouter.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owners-model");
 
 router.get("/", function (req, res) {
   res.send("Owner Router");
 });
+//$env:NODE_ENV="development"
+console.log(process.env.NODE_ENV);
+if(process.env === "development"){
+  console.log("This is working ");
+}
+
+router.post("/create",function(req,res){
+  res.send("Owner Created");
+})
 
 module.exports = router;
