27cb5bfe4ae46d0c039e09ce9dd3991bc58d65db
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-11-10 23:17:32 +0530

    projrct

--- project/config/mongoose-connection.js
+++ project/config/mongoose-connection.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const dbgr = require("debug")("development:mongoose");
 
 mongoose.connect("mongodb://localhost:27017/config").then(function () {
-  console.log("Connected to database");
+  // console.log("Connected to database");
+  dbgr("Connected to database");
 })
 .catch(function (error) {
   console.log("Error connecting to database");
