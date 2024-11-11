82fb959b292466b2ad26ced6ab9b7fec2e99a706
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-11-11 17:23:07 +0530

    project

--- project/routes/ownerRouter.js
+++ project/routes/ownerRouter.js
@@ -2,17 +2,22 @@ const express = require("express");
 const router = express.Router();
 const ownerModel = require("../models/owners-model");
 
-router.get("/", function (req, res) {
-  res.send("Owner Router");
-});
-//$env:NODE_ENV="development"
-console.log(process.env.NODE_ENV);
-if(process.env === "development"){
-  console.log("This is working ");
+if (process.env.NODE_ENV === "development") {
+  // console.log("This is working ");
+
+  router.post("/create", async function (req, res) {
+    let owners = await ownerModel.find();
+    if (owners.length > 0) {
+      return res.status(503).send("Owner already exists");
+    }
+    res.send("we can creat Owner ");
+  });
 }
 
-router.post("/create",function(req,res){
-  res.send("Owner Created");
-})
+router.get("/", function (req, res) {
+  res.send("Owner Routerrr");
+});
+//$env:NODE_ENV="development"
+// console.log(process.env.NODE_ENV);
 
 module.exports = router;
