f2bcd07c48498ae80bf8d4d7f9590eb89eb44978
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2025-03-10 22:47:48 +0530

    project

--- projectchiaorbac/src/utils/ApiError.js
+++ projectchiaorbac/src/utils/ApiError.js
@@ -1,16 +1,23 @@
-class ApiError extends Error{
-    constructor(
-        statusCode,
-        messahe ="Somthing went wrong",
-        error=[],
-        statck =""
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Somthing went wrong",
+    error = [],
+    statck = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.error = error;
+    this.data = null;
+    this.succes = false;
+    this.message = message;
 
-    ){
-        super(message)
-        this.statusCode = statusCode;
-        this.error = error;
-        this.data = null
-        this.succes=false;
-        this.message = message;
+    if (statck) {
+      this.stack = statck;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
+
+export {ApiError}
