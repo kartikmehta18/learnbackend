be7a84a63457cb3879069f8decc0d509717efceb
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2025-03-10 19:47:24 +0530

    project

--- projectchiaorbac/src/index.js
+++ projectchiaorbac/src/index.js
@@ -1,5 +1,29 @@
-import  { express } from 'express';
+//require("dotenv").config({path: "./env"});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { config } from "dotenv";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-app.get("/",(req ,res)=>{
-res.send("hello");
-})
+const __dirname = dirname(fileURLToPath(import.meta.url));
+config();
+dotenv.config({ path: "./env" });
+
+connectDB();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`$(process.env.MONGO_URI)/${DB_NAME}`);
+//     app.on("error", () => {
+//       console.log("error", error);
+//       throw err;
+//     });
+
+//     app.listen(process.env.PORT,()=>{
+//         console.log("server is running on port", process.env.PORT);
+//     })
+//   } catch (error) {
+//     console.log("ERROR", error);
+//     throw err;
+//   }
+// })();
