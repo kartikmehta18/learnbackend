d79c6f3409aa77f89db3a7f0f4247e02a2789265
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2025-04-21 14:51:02 +0530

    Enhance user routes with additional endpoints for password management, user profile updates, and watch history retrieval

--- projectchiaorbac/src/routes/user.routes.js
+++ projectchiaorbac/src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   loginUser,
   refreshAccessToken,
+  changeCurrecntPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -21,5 +28,18 @@ router.route("/login").post(loginUser);
 ///seciured rout
 
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changeCurrecntPassword);
+router.route("/current-password").post(verifyJWT, getCurrentUser);
+router.route("/updat-accout").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avata"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverimage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 export default router;
